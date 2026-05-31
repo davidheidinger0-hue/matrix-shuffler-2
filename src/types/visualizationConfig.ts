428473eec3a9e8c0c12412d197adc5407b6e28eb
Auer Lukas <lukas.auer@student.tugraz.at>
2026-05-31 16:31:26 +0200

    revert old changes

--- src/types/visualizationConfig.ts
+++ src/types/visualizationConfig.ts
@@ -2,7 +2,7 @@ export interface VisualizationConfig {
   width: number
   height: number
   margin: { top: number; right: number; bottom: number; left: number }
-  colorScheme: 'blues' | 'greens' | 'heatmap' | 'bertifier'
+  colorScheme: 'blues' | 'greens' | 'heatmap'
   cellShape: 'rect' | 'circle'
   showLabels: boolean
   encoding: VisualizationEncoding
