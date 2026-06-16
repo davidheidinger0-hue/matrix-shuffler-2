321974d593f44daa1c3339dd02c710af082aa7ef
lumen <christina.vogel@student.tugraz.at>
2026-06-16 23:41:57 +0200

    fix vue component naming problem

--- src/components/Minimap.vue
+++ src/components/Minimap.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import { ref, onMounted, onUnmounted, watch, nextTick, reactive } from 'vue'
 
+defineOptions({
+  name: 'MiniMap'
+})
+
 const props = defineProps<{
   wrapper: HTMLDivElement | null
   canvas: HTMLCanvasElement | null
